691f829610f91ed3d35b1d27ae753f16e92f5e74
Ixhika <106822722+Ixhika@users.noreply.github.com>
2023-09-10 14:01:02 +0530

    Update L17

--- Codes/L_17_LetterCombinations.java
+++ Codes/L_17_LetterCombinations.java
@@ -11,6 +11,7 @@ public class L_17_LetterCombinations {
         if(digits.length() == 0){
             return new ArrayList<>();
         }
+        // Storing all the combinations in a map
         HashMap<Character,String> map = new HashMap<>();
         map.put('2',"abc");
         map.put('3',"def");
@@ -27,15 +28,20 @@ public class L_17_LetterCombinations {
     }
 
 public void backtrack(String digits,HashMap<Character,String> map,List<String> res,StringBuilder temp,int idx){
+    // Base case for each condition to terminate and get stored in res
         if(idx == digits.length()){
             res.add(temp.toString());
             return;
         }
+        // Getting all the values from the map and iterating over them
         String currString = map.get(digits.charAt(idx));
         for(int i=0;i<currString.length();i++){
+
+            // Addig them to a temp list that will be checking all the possible combinations 
+            /* str = "23"  res = "ad" ,"ae" , "af" , "bd" , ......."cf" */
              temp.append(currString.charAt(i));
             backtrack(digits, map, res,temp, idx+1);
-            temp.deleteCharAt(temp.length()-1);
+            temp.deleteCharAt(temp.length()-1);   //Once combination is made backtracking to previous added element by removing the last element and making space for adding another element to the list
         }
     }
 }
