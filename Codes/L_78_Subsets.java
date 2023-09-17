22b2d3837fb8bd44241b858838e7c2999fe77fd8
Ixhika <106822722+Ixhika@users.noreply.github.com>
2023-09-17 13:47:21 +0530

    leetcode

---
+++ Codes/L_78_Subsets.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Time Complexity = O(n*2^n)
+// Space Complexity = O(n)
+public class L_78_Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res,new ArrayList<>(),nums,0);
+        return res;
+    }
+    public void backtrack(List<List<Integer>> res, List<Integer> temp, int[] nums,int start){
+        // this condition is checking out of bounds conditon
+        if(start >= nums.length){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        // first line adding the empty list which is also counted as a subset
+        res.add(new ArrayList<>(temp));
+        // traversing through the array and adding elements one by one 
+        for(int i=start;i<nums.length;i++){
+            temp.add(nums[i]);
+            backtrack(res,temp,nums,i+1);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
---
+++ Codes/L_90_SubsetsII.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class L_90_SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res,new ArrayList<>(),nums,0);
+        return res;
+    }
+    public void backtrack(List<List<Integer>> res, List<Integer> temp, int[] nums,int start){
+        res.add(new ArrayList<>(temp));
+        for(int i=start;i<nums.length;i++){
+            // to skip duplicate elements
+            if(i > start && nums[i] == nums[i-1]){
+                continue;
+            }
+            temp.add(nums[i]);
+            backtrack(res,temp,nums,i+1);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
